1328ae521e93de1a3754a06f4f8c0391b04c5f51
Aman14-ai <amancode2005@gmail.com>
2025-05-13 22:53:31 +0530

    second

--- sanity.types.ts
+++ sanity.types.ts
@@ -272,6 +272,9 @@ export type STARTUPS_BY_AUTHOR_QUERYResult = Array<{
   category: string | null;
   image: string | null;
 }>;
+// Variable: PLAYLIST_BY_SLUG_QUERY
+// Query: *[_type == playlist && slug.current == $slug][0] {    _id,    title,    slug,    select[] -> {      _id,      _createdAt,      title,      slug,      author -> { _id, name , slug , image , bio },      views,      description,      category,      image,      pitch    }  }
+export type PLAYLIST_BY_SLUG_QUERYResult = null;
 
 // Query TypeMap
 import "@sanity/client";
@@ -283,5 +286,6 @@ declare module "@sanity/client" {
     "\n    *[_type == 'author' && id == $id][0]{\n      _id,\n      id,\n      name,\n      username , \n      image , \n      bio,\n      email,\n    } \n  ": AUTHOR_BY_GITHUB_ID_QUERYResult;
     "\n    *[_type == 'author' && _id == $id][0]{\n      _id,\n      id,\n      name,\n      username , \n      image , \n      bio,\n      email,\n    } \n  ": AUTHOR_BY_OWN_ID_QUERYResult;
     "*[_type == 'startup' && author._ref == $id] | order(_createdAt desc) {\n      _id,\n      _createdAt,\n      title,\n      slug,\n      author -> { _id, name },\n      views,\n      description,\n      category,\n      image,\n    }": STARTUPS_BY_AUTHOR_QUERYResult;
+    "*[_type == playlist && slug.current == $slug][0] {\n    _id,\n    title,\n    slug,\n    select[] -> {\n      _id,\n      _createdAt,\n      title,\n      slug,\n      author -> { _id, name , slug , image , bio },\n      views,\n      description,\n      category,\n      image,\n      pitch\n    }\n  }": PLAYLIST_BY_SLUG_QUERYResult;
   }
 }
